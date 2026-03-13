e6c91b8fa552fef4d9979ff2ac2cd5030664c965
semo <seongmo95@gmail.com>
2026-03-13 23:27:31 +0900

    docs: update daily log(2026-03-13)

--- Source/ClickerGame/Public/Systems/UI/Settings/ClickerUISettings.h
+++ Source/ClickerGame/Public/Systems/UI/Settings/ClickerUISettings.h
@@ -6,6 +6,7 @@
 #include "Engine/DataAsset.h"
 #include "ClickerUISettings.generated.h"
 
+class UClickerHUDRootWidgetBase;
 class UUserWidget;
 class UIdleRewardTextWidget;
 class UClickFloatingTextWidget;
