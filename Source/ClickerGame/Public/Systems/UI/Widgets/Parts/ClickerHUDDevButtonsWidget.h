7bef9a4afa0dce9d8857213351b000da6f22d64c
semo <seongmo95@gmail.com>
2026-04-03 23:29:47 +0900

    refactor: WIP - implement WBP_DevButtons cycle

---
+++ Source/ClickerGame/Public/Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Systems/UI/Widgets/Parts/ClickerHUDPartWidgetBase.h"
+#include "ClickerHUDDevButtonsWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLICKERGAME_API UClickerHUDDevButtonsWidget : public UClickerHUDPartWidgetBase
+{
+	GENERATED_BODY()
+	
+};
