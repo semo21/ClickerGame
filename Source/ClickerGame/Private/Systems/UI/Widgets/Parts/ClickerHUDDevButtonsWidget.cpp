d1c13d88cc7da6628742ada128589785905b439e
semo <seongmo95@gmail.com>
2026-04-27 23:52:38 +0900

    refactor: WIP - WBP_DevButtons structure

--- Source/ClickerGame/Private/Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.cpp
+++ Source/ClickerGame/Private/Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.cpp
@@ -3,3 +3,20 @@
 
 #include "Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.h"
 
+#include "Systems/UI/ClickerUISubsystem.h"
+#include "Gameplay/Player/MyPlayerController.h"
+#include "Systems/UI/Widgets/ActionButton/ActionButtonWidgetBase.h"
+
+void UClickerHUDDevButtonsWidget::InitializePart_Implementation(UClickerUISubsystem* InUI, AMyPlayerController* InPC) {
+	Super::InitializePart_Implementation(InUI, InPC);
+	UE_LOG(LogTemp, Warning, TEXT("UClickerHUDDevButtonsWidget::InitializePart_Implementation - Initializing dev buttons"));
+	if (Btn_Save && SaveActionTag.IsValid()) {
+		Btn_Save->InitializeButton(InPC, InUI, SaveActionTag);
+		UE_LOG(LogTemp, Warning, TEXT("UClickerHUDDevButtonsWidget::InitializePart_Implementation - Initialized Save button with tag: %s"), *SaveActionTag.ToString());
+	}
+	if (Btn_Load && LoadActionTag.IsValid()) {
+		Btn_Load->InitializeButton(InPC, InUI, LoadActionTag);
+		UE_LOG(LogTemp, Warning, TEXT("UClickerHUDDevButtonsWidget::InitializePart_Implementation - Initialized Load button with tag %s"), *LoadActionTag.ToString());
+	}
+	
+}
--- Source/ClickerGame/Public/Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.h
+++ Source/ClickerGame/Public/Systems/UI/Widgets/Parts/ClickerHUDDevButtonsWidget.h
@@ -3,9 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Systems/UI/Widgets/Parts/ClickerHUDPartWidgetBase.h"
+
 #include "ClickerHUDDevButtonsWidget.generated.h"
 
+class UActionButtonWidgetBase;
+class UClickerUISubsystem;
+class AMyPlayerController;
 /**
  * 
  */
@@ -14,4 +19,19 @@ class CLICKERGAME_API UClickerHUDDevButtonsWidget : public UClickerHUDPartWidget
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void InitializePart_Implementation(UClickerUISubsystem* InUI, AMyPlayerController* InPC) override;
+
+protected:
+	UPROPERTY(meta=(BindWidget), BlueprintReadOnly)
+	TObjectPtr<UActionButtonWidgetBase> Btn_Save;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadOnly)
+	TObjectPtr<UActionButtonWidgetBase> Btn_Load;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Actions")
+	FGameplayTag SaveActionTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Actions")
+	FGameplayTag LoadActionTag;
 };
